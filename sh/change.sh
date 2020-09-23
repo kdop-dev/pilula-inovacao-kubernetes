8682a8c422dcf35ad60bea4460770116db7d80ab
leonardoosse <leonardo.osse@gmail.com>
2020-09-23 04:38:17 -0300

    remove set env

--- sh/change.sh
+++ sh/change.sh
@@ -1,4 +1,4 @@
-mkdir -p ~/work/inovacao
+#!/bin/bash
 cd ~/work/inovacao
 
 if [ -z "$myNamespace" ]
@@ -9,6 +9,10 @@ else
     sed -i -e  "s/<seu namespace>/$myNamespace/g" back-app/helm/back-app/values.yaml    
     sed -i -e  "s/<seu namespace>/$myNamespace/g" cert-app/helm/cert-app/values.yaml
     sed -i -e  "s/<seu namespace>/$myNamespace/g" ~/sh/preparacao.sh
+    sed -i -e  "s/<seu namespace>/$myNamespace/g" ~/02_Kubernetes.ipynb
+    sed -i -e  "s/<seu namespace>/$myNamespace/g" ~/03_Helm.ipynb
+    sed -i -e  "s/<seu namespace>/$myNamespace/g" ~/04_Back.ipynb
+    sed -i -e  "s/<seu namespace>/$myNamespace/g" ~/05_Cert.ipynb    
 fi
 
 echo $PWD
